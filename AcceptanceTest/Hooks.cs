24780a89ba6ae944851b9c549db2cbc869d020f9
behna <behnamsa96@outlook.com>
2023-09-13 01:00:55 +0330

    Add Hooks class
    - Inject HttpClient to the Scenario Context of Specflow

---
+++ AcceptanceTest/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using BoDi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTest;
+
+[Binding]
+public class Hooks
+{
+    private readonly ScenarioContext _scenarioContext;
+
+    public Hooks(ScenarioContext scenarioContext) => _scenarioContext = scenarioContext;
+
+
+    [BeforeScenario]
+    public void CreateApplication()
+    {
+        var application = new WebApplicationFactory<Program>();
+
+        _scenarioContext.Add("httpClient", application.CreateClient());
+    }
+}
