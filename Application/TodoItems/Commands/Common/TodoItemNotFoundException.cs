7ff06f999506531adfd14d8fc21ad9b12d3756e5
behna <behnamsa96@outlook.com>
2023-09-14 19:16:02 +0330

    Delete redundant codes

--- Application/TodoItems/Commands/Common/TodoItemNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Common.Exception;
-
-namespace Application.TodoItems.Commands.Common
-{
-    public class TodoItemNotFoundException : Exception, IException
-    {
-        public TodoItemNotFoundException() : base("TodoItem not found.") { }
-    }
-}
--- Application/TodoItems/Queries/GetTodayTodoItems/GetTodayTodoItemsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Application.TodoItems.Queries.GetTodayTodoItems
-{
-    public class GetTodayTodoItemsQuery : IRequest<TodoItemViewModel[]>
-    {
-        public static GetTodayTodoItemsQuery Default => new();
-    }
-}
