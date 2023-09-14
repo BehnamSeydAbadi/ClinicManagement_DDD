53a72c7811f2e0f82c3101a0beb2a33423a54ec0
behna <behnamsa96@outlook.com>
2023-09-14 16:07:58 +0330

    Delete Application.IntegrationTest

---
+++ Application.IntegrationTest/Appointment/Tests.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Application.IntegrationTest.Appointment;
+
+public class Tests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly ISender _mediator;
+
+    public Tests(WebApplicationFactory<Program> webAppFactory)
+    {
+        _mediator = webAppFactory.Services.GetService<ISender>()!;
+    }
+
+    [Fact]
+    public async Task Schedule_Successful()
+    {
+        var id = await _mediator.Send()
+    }
+}
